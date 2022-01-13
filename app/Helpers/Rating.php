851c34a7aa39cebee8266df2f6268f589a93df66
mobydimk <mobydimk@gmail.com>
2022-01-13 23:31:34 +0400

    empty rating value by default

--- app/Helpers/Rating.php
+++ app/Helpers/Rating.php
@@ -4,7 +4,9 @@ namespace App\Helpers;
 class Rating {
 
     public static function rangeOptions($max) {
-        $res = [];
+        $res = [
+            '' => ''
+        ];
         for($i=1; $i<=$max; $i++) {
             $res[''.$i] = $i.' / '.$max;
         }
