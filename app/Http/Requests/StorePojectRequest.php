2bcc1f7592a7ccab97aae8116472356050fd882b
mobydimk <mobydimk@gmail.com>
2022-01-13 23:16:38 +0400

    make finalized project fields mandatory

--- app/Http/Requests/StorePojectRequest.php
+++ app/Http/Requests/StorePojectRequest.php
@@ -26,12 +26,15 @@ class StorePojectRequest extends FormRequest
         return [
             'name' => 'required|max:255',
             'school_id' => 'required|exists:schools,id',
-            'grade_id' => 'exists:grades,id',
-            'team_girls' => 'nullable|integer|min:0|max:1000',
-            'team_boys' => 'nullable|integer|min:0|max:1000',
-            'team_not_provided' => 'nullable|integer|min:0|max:1000',
-            'presentation_file' => 'mimes:pdf',
-            'image_file' => 'mimes:jpg,jpeg,png,gif',
+            'grade_id' => 'required_if:finalize,1|exists:grades,id',
+            'team_girls' => 'required_if:finalize,1|nullable|integer|min:0|max:1000',
+            'team_boys' => 'required_if:finalize,1|nullable|integer|min:0|max:1000',
+            'team_not_provided' => 'required_if:finalize,1|nullable|integer|min:0|max:1000',
+            'presentation_file' => 'required_if:finalize,1|mimes:pdf',
+            'zip_file' => 'required_if:finalize,1|mimes:zip',
+            'image_file' => 'required_if:finalize,1|mimes:jpg,jpeg,png,gif',
+            'description' => 'required_if:finalize,1',
+            'video_url' => 'required_if:finalize,1',
         ];
     }
 }
--- resources/lang/en/validation.php
+++ resources/lang/en/validation.php
@@ -108,7 +108,7 @@ return [
     'prohibits' => 'The :attribute field prohibits :other from being present.',
     'regex' => 'The :attribute format is invalid.',
     'required' => 'The :attribute field is required.',
-    'required_if' => 'The :attribute field is required when :other is :value.',
+    'required_if' => 'The :attribute field is required.',
     'required_unless' => 'The :attribute field is required unless :other is in :values.',
     'required_with' => 'The :attribute field is required when :values is present.',
     'required_with_all' => 'The :attribute field is required when :values are present.',
