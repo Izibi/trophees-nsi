13b534f72456f082f3cdceebda43fc4a1a51512a
mobydimk <mobydimk@gmail.com>
2022-01-25 13:51:15 +0400

    separate contest status message

---
+++ resources/views/projects/contest-status/teacher.blade.php
@@ -0,0 +1,13 @@
+<h3>
+@if($contest->status == 'preparing')
+Le dépôt de projets n'est pas encore ouvert.
+@elseif($contest->status == 'open')
+La période de dépôt de projets est en cours
+@elseif($contest->status == 'grading')
+Le dépôt de projets est clôturé, les projets sont évalués par le jury régional.
+@elseif($contest->status == 'deliberating')
+Le dépôt de projets est clôturé, le jury régional délibère.
+@elseif($contest->status == 'closed')
+Le dépôt de projets est clôturé, le jury a terminé la délibération.
+@endif
+</h3>
--- resources/views/projects/index/jury.blade.php
+++ resources/views/projects/index/jury.blade.php
@@ -1,7 +1,7 @@
 @extends('layout')
 
 @section('content')
-    @include('projects.contest-status')
+    @include('projects.contest-status.jury')
 
     <div class="card mt-3 mb-3">
         <div class="card-header">
