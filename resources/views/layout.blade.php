362e6af47ce1acfdb3249d4b6453afb8df74c761
mobydimk <mobydimk@gmail.com>
2022-01-26 00:41:56 +0400

    title and favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,12 +4,14 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico">
         <meta name="theme-color" content="#ffffff">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="{{ mix('js/vendor.js') }}"></script>
         <script src="{{ mix('js/app.js') }}"></script>
         <script src="{{ mix('js/manifest.js') }}"></script>
+        <title>Trophés NSI - dépôt de projets</title>
         @yield('head')
     </head>
 
