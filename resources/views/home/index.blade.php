65f679019c0ccfc0b538480f1a4981f334894742
mobydimk <mobydimk@gmail.com>
2022-02-04 07:47:57 +0400

    home page update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -7,9 +7,6 @@
     @endif
 
     @if (!Auth::check())
-        <p>Les projets pour les Trophées NSI doivent être déposés par l'enseignant. Si vous êtes enseignant, vous pouvez vous inscrire ou vous connecter ci-dessous.</p>
-        <p>Si vous êtes élève, contactez votre professeur de NSI pour l'informer que vous souhaitez que votre équipe participe aux Trophées NSI.</p>
-
         <div class="justify-content-center">
             <a class="btn btn-secondary" href="/login">Se connecter ou créer un compte</a>
         </div>
