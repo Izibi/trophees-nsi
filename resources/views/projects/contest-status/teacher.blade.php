3699ec5c92757011be71d0d3c046632d7c36c177
Admin France-IOI <info@france-ioi.org>
2023-05-25 09:33:33 +0000

    Text update

--- resources/views/projects/contest-status/teacher.blade.php
+++ resources/views/projects/contest-status/teacher.blade.php
@@ -6,7 +6,7 @@ La période de dépôt de projets est en cours
 @elseif($contest->status == 'grading')
 Le dépôt de projets est clôturé, les projets sont évalués par le jury régional.
 @elseif($contest->status == 'deliberating')
-Le dépôt de projets est clôturé, le jury régional délibère.
+Le dépôt de projets est clôturé, le jury national délibère.
 @elseif($contest->status == 'closed')
 Le dépôt de projets est clôturé, le jury a terminé la délibération.
 @endif
