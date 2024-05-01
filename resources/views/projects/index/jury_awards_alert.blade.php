8f9c15decf5cdb8b3f64cbac4d93807ca2c513d6
Michel Blockelet <blockelet@crans.org>
2024-05-01 14:13:22 +0100

    Fix translations for jury awards alert

--- resources/views/projects/index/jury_awards_alert.blade.php
+++ resources/views/projects/index/jury_awards_alert.blade.php
@@ -1,27 +1,27 @@
 @if($awards_count !== false)
     @if($awards_count['award_mixed'] > $awards_limit['award_mixed'])
         <div class="alert alert-danger">
-            award_mixed quantity exceeded ({{ $awards_count['award_mixed'] }} of {{ $awards_limit['award_mixed'] }})
+            Limite d'attribution du prix mixed atteinte ({{ $awards_count['award_mixed'] }} sur {{ $awards_limit['award_mixed'] }})
         </div>
     @endif
     @if($awards_count['award_citizenship'] > $awards_limit['award_citizenship'])
         <div class="alert alert-danger">
-            award_citizenship quantity exceeded ({{ $awards_count['award_citizenship'] }} of {{ $awards_limit['award_citizenship'] }})
+            Limite d'attribution du prix citizenship atteinte ({{ $awards_count['award_citizenship'] }} sur {{ $awards_limit['award_citizenship'] }})
         </div>
     @endif
     @if($awards_count['award_engineering'] > $awards_limit['award_engineering'])
         <div class="alert alert-danger">
-            award_engineering quantity exceeded ({{ $awards_count['award_engineering'] }} of {{ $awards_limit['award_engineering'] }})
+            Limite d'attribution du prix théatique atteinte ({{ $awards_count['award_engineering'] }} sur {{ $awards_limit['award_engineering'] }})
         </div>
     @endif
     @if($awards_count['award_heart'] > $awards_limit['award_heart'])
         <div class="alert alert-danger">
-            award_heart quantity exceeded ({{ $awards_count['award_heart'] }} of {{ $awards_limit['award_heart'] }})
+            Limite d'attribution du prix spécial du jury atteinte ({{ $awards_count['award_heart'] }} sur {{ $awards_limit['award_heart'] }})
         </div>
     @endif
     @if($awards_count['award_originality'] > $awards_limit['award_originality'])
         <div class="alert alert-danger">
-            award_originality quantity exceeded ({{ $awards_count['award_originality'] }} of {{ $awards_limit['award_originality'] }})
+            Limite d'attribution du prix de la créativité atteinte ({{ $awards_count['award_originality'] }} sur {{ $awards_limit['award_originality'] }})
         </div>
     @endif
 @endif
