d59084940bc476ae401199196f18bf4d2b1b7c6e
Michel Blockelet <blockelet@crans.org>
2025-11-04 00:55:07 +0000

    Update ressources

--- resources/views/ressources/index.blade.php
+++ resources/views/ressources/index.blade.php
@@ -4,29 +4,29 @@
     <div class="container">
         <h1>Ressources</h1>
 
-        <p>Sur cette page, vous trouverez d'ici peu diverses ressources utiles pour la participation aux Trophées NSI.</p>
+        <p>Sur cette page, vous trouverez diverses ressources utiles pour la participation aux Trophées NSI.</p>
 
-        <!--
         <div class="mt-4">
-            <h2>Liens utiles</h2>
             <ul class="list-group">
                 <li class="list-group-item">
-                    <a href="#" target="_blank">Règlement</a>
+                    <a href="https://trophees-nsi.fr/media/pages/ressources/50e1b135bd-1762177827/2025.11.03_trophees-nsi-2026_reglement.pdf" target="_blank">Règlement 2026</a>
                 </li>
                 <li class="list-group-item">
-                    <a href="#" target="_blank">Modèle du dossier de candidature</a>
+                    <a href="https://trophees-nsi.fr/media/pages/ressources/26fc5375b6-1762178657/2026_autorisation-droits-d-auteur-et-droit-a-l-image-eleve-mineur.pdf" target="_blank">Autorisation à compléter et à signer pour un élève mineur</a>
                 </li>
                 <li class="list-group-item">
-                    <a href="#" target="_blank">Modèle du dossier technique</a>
+                    <a href="https://trophees-nsi.fr/media/pages/ressources/72d9ac731d-1762178657/2026_autorisation-droits-d-auteur-et-droit-a-l-image-eleve-majeur.pdf" target="_blank">Autorisation à compléter et à signer pour un élève majeur</a>
                 </li>
                 <li class="list-group-item">
-                    <a href="#" target="_blank">Webinaire d'utilisation de la forge</a>
+                    <a href="/projects/exemple" target="_blank">Modèle du dossier de candidature</a>
                 </li>
                 <li class="list-group-item">
-                    <a href="#" target="_blank">Autorisations parentales à signer</a>
+                    <a href="https://forge.apps.education.fr/trophees-nsi-2026/modele" target="_blank">Modèle du dossier technique</a>
+                </li>
+                <li class="list-group-item">
+                    <i>Webinaire d'utilisation de la forge - à venir en fin de semaine</i>
                 </li>
             </ul>
         </div>
--->
     </div>
 @endsection
